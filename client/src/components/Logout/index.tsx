86a0b2ccee6b5ab9f9f2c9e8188af96a7fbc8f65
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-08-19 14:52:50 -0700

    fix[client]: moved client from public to client folder

---
+++ client/src/components/Logout/index.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import { BiPowerOff } from 'react-icons/bi';
+import { logoutRoute } from '../../utils/APIRoutes';
+import styles from './Logout.module.scss';
+import { toast } from 'react-toastify';
+import axiosInstance from '../../utils/axiosInstance';
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    const { status } = await axiosInstance.post(`${logoutRoute}`);
+    if (status === 200) {
+      toast.success('Logged out successfully');
+      localStorage.clear();
+      navigate('/login');
+    }
+  };
+
+  return (
+    <>
+      <button className={styles.button} onClick={handleClick}>
+        <span>Log Out</span> <BiPowerOff />
+      </button>
+    </>
+  );
+};
+
+export default Logout;
